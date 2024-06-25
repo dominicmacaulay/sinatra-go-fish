09d3b819b4920d746ed5f43fc96003de9ef49b41
dominicmacaulay <dominic.62502@gmail.com>
2024-06-25 09:52:41 -0400

    get a passing test for authorization

--- lib/server.rb
+++ lib/server.rb
@@ -28,7 +28,10 @@ class Server < Sinatra::Base # rubocop:disable Style/Documentation
     session[:current_player] = player
     game.add_player(player)
     respond_to do |f|
-      f.html { redirect '/game' }
+      f.html do
+        session['HTTP_AUTHORIZATION'] = "Basic #{Base64.encode64("#{player_api_key}:X")}"
+        redirect '/game'
+      end
       f.json { json api_key: player_api_key }
     end
   end
@@ -36,13 +39,27 @@ class Server < Sinatra::Base # rubocop:disable Style/Documentation
   get '/game' do
     redirect '/' if game.empty?
     # TODO: if the passed in params contain an api key that is recognized, continue else send 401
+    key = session['HTTP_AUTHORIZATION'].nil? ? request.env['HTTP_AUTHORIZATION'] : session['HTTP_AUTHORIZATION']
+    halt 401, 'Unauthorized' unless valid_api_key?(key)
+
     respond_to do |f|
       f.html { slim :game, locals: { game: game, current_player: session[:current_player] } }
       f.json { json players: game.players }
     end
   end
 
+  private
+
   def make_api_key
     SecureRandom.hex(10)
+  end
+
+  def valid_api_key?(key)
+    credentials = key.split(' ').last
+    api_key = Base64.decode64(credentials).split(':').first
+    game.players.each do |player|
+      return true if api_key.include? player.api_key
+    end
+    false
   end
 end
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -33,6 +33,20 @@ RSpec.describe Server do # rubocop:disable Metrics/BlockLength
       expect(session).to have_css('b', text: player_name)
     end
     expect(session2).to have_content('Player 1')
+    session1.driver.refresh
+    expect(session1).to have_content('Player 2')
+  end
+
+  it 'does not show opponents in bold and reveals the personal api only' do
+    session1 = Capybara::Session.new(:rack_test, Server.new)
+    session2 = Capybara::Session.new(:rack_test, Server.new)
+    [session1, session2].each_with_index do |session, index|
+      player_name = "Player #{index + 1}"
+      session.visit '/'
+      session.fill_in :name, with: player_name
+      session.click_on 'Join'
+    end
+    expect(session2).to have_content('Player 1')
     expect(session2).not_to have_css('b', text: 'Player 1')
     expect(session2).to have_content('api key', count: 1)
     session1.driver.refresh
@@ -59,5 +73,15 @@ RSpec.describe Server do
       'HTTP_ACCEPT' => 'application/json'
     }
     expect(JSON.parse(last_response.body).keys).to include 'players'
+  end
+
+  it 'returns an error if the key is not authorized' do
+    api_key = '12345'
+    expect(api_key).not_to be_nil
+    get '/game', nil, {
+      'HTTP_AUTHORIZATION' => "Basic #{Base64.encode64("#{api_key}:X")}",
+      'HTTP_ACCEPT' => 'application/json'
+    }
+    expect(last_response.status).to eql 401
   end
 end
